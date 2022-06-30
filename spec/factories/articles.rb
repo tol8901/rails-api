8b8a9759b991dd5eb1f1c15fbdb2a1cfc1ee3781
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-06-30 22:02:08 +0300

    Add title, content and slug validation RSpec tests (check - not blank; and for slug - uniqueness)

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "Sample article" }
-    content { "Sample content" }
-    slug { "sample-article" }
+    sequence(:title) { |n| "Sample article #{n}" }
+    content { 'Sample content' }
+    sequence(:slug) { |n| "sample-article-#{n}" }
   end
 end
