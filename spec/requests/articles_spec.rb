61087542251adb33e92f880acc1c0a57aa8736c3
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-01 22:06:43 +0300

    [Section_6:25] Serializing Articles

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -11,16 +11,17 @@ RSpec.describe ArticlesController do
     it 'returns a proper JSON' do
       article = create :article
       get '/articles'
-      body = JSON.parse(response.body)
+      body = JSON.parse(response.body).deep_symbolize_keys
+      # pp body - logging
       expect(body).to eq(
                         data: [
                             {
-                            id: article.id,
+                            id: article.id.to_s,
                             type: 'articles',
                             attributes: {
                               title: article.title,
                               content: article.content,
-                              slug: artcle.slug
+                              slug: article.slug
                             }
                           }
                         ]
