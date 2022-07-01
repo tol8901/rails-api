e4e51c3606f98eb496bb5e95325266e22e6c15db
Anatolii Kravchenko <anatoliy.kravchenko1991@gmail.com>
2022-07-01 20:43:42 +0300

    [Assignment_4] Article's details route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,2 @@
 class ArticlesController < ApplicationController
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get '/articles', to: 'articles#index'
-  resources :articles, only: [:index]
+  resources :articles, only: %i[index show]
 end
